b3a2a413ffd7dfd3dfcec95959456335ba61066c
Chaitanya-modi <chaitanyamodi711@gmail.com>
2024-04-18 00:36:20 +0530

    added stuff

--- api_main.py
+++ api_main.py
@@ -1,39 +1,43 @@
 import asyncio
 from Utils.api_utils import PlanetData 
+import pathlib
+
 async def main():
-    credentials = {'API_KEY': 'PLAKbc1515084b754a179875fd75af529072'} # zoya's
-    geom = {
-            "coordinates": [
-            [
-              [80.753117, 27.960661], 
-              [80.754393, 27.961983], 
-              [80.755917, 27.961016], 
-              [80.754066, 27.959855], 
-              [80.753117, 27.960661]
-              ]
-            ],
-            "type": "Polygon"
+  credentials = {'API_KEY': 'PLAK28bdd18f2ecb4022b2599de197421f8d'}  # shivangi's
+  geom = {
+          "coordinates": [[
+            [78.326991, 27.037117], 
+            [78.326841, 27.036944], 
+            [78.327184, 27.036723], 
+            [78.327309, 27.036881], 
+            [78.326991, 27.037117]
+          ]],
+          "type": "Polygon"
           }  
-      
-    # an instance of class in utils
-    planet_data = PlanetData(
-        credentials=credentials,
-        clear_percent_filter_value=(80, 100),  # clear images between 80% to 100%
-        date_range={'gte': '2024-03-14', 'lte': '2024-04-01'}, 
-        cloud_cover_filter_value=(0, 20),  # max cloud cover of 20%
-        item_types=['PSScene'],  # satellite images
-        limit=10,  # limit the number of images to retrieve
-        directory="/Users/chaitanyamodi/Downloads/agri-informatics/Images/"  # directory to save downloaded stuff
-    )
+
+    #  an instance of the class in utils
+  planet_data = PlanetData(
+  credentials=credentials,
+  clear_percent_filter_value=(80, 100),  # clear images between 80% to 100%
+  date_range={'gte': '2024-03-30', 'lte': '2024-04-04'}, 
+  cloud_cover_filter_value=(0, 20),  # max cloud cover of 20%
+  item_types=['PSScene'],  # satellite images
+  limit=10,  # limit the number of images to retrieve
+  directory="./Images/"  # directory to save downloaded stuff
+)
     
-    results, item_list, search_df = await planet_data.download_multiple_assets(geom=geom, asset_type_id='ortho_analytic_8b_sr')
-    
-    print("Downloaded assets:")
-    print(results)
-    print("Items list:")
-    print(item_list)
-    print("Search DataFrame:")
-    print(search_df)
+  results, item_list, search_df = await planet_data.download_multiple_assets(geom=geom, asset_type_id='ortho_analytic_8b_sr')
+
+  print("Downloaded assets:")
+  print(results)
+  print("Items list:")
+  print(item_list)
+  print("Search DataFrame:")
+  print(search_df)
+
+  tif_files = [result for result in results if pathlib.Path(result).suffix == '.tif']
+  return tif_files
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    tif_images = asyncio.run(main())
+    print("TIFF Images:", tif_images)
