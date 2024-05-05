4ac1ba384225c26ee68228d096d5152eaa90dbee
shivangiag08 <shivangiag08@gmail.com>
2024-05-05 20:31:15 +0530

    "finally working without errors without calling api multiple times woohoo :)"

--- Utils/database_utils.py
+++ Utils/database_utils.py
@@ -1,36 +1,50 @@
 import psycopg2
 from geojson import Polygon
 
-def check_area_coverage(geojson_polygon, date, connection_params):
+def check_area_coverage(polygon, date, connection_params):
+    date = '-'.join([date[:4], date[4:6], date[6:]])
+
     # Convert GeoJSON to WKT (Well-Known Text)
-    geometry_wkt = str(Polygon(geojson_polygon['coordinates']))
-    
+    wkt = f"POLYGON(({', '.join([' '.join(map(str, coord)) for coord in polygon['coordinates'][0]])}))"
+
     # Connect to the database
     conn = psycopg2.connect(**connection_params)
     cur = conn.cursor()
     
     # SQL query to check for existence of the data and get the image path
-    cur.execute(
-        "SELECT image_path FROM satellite_images WHERE geometry && ST_GeomFromText(%s, 4326) AND acquisition_date = %s",
-        (geometry_wkt, date)
-    )
+    cur.execute("SELECT image_path FROM satellite_images WHERE ST_Intersects(geometry, ST_GeomFromText(%s, 4326)) AND acquisition_date = %s",(wkt, date))
     result = cur.fetchone()
+
+    print("\nresult from query:", result)
     cur.close()
     conn.close()
     return result[0] if result else None
 
-def add_new_image(tile_id, acquisition_date, geojson_polygon, image_path, connection_params):
+def add_new_image(tile_id, acquisition_date, coordinates, image_path, connection_params):
+    # Convert date string from 'YYYYMMDD' to a date object
+    acquisition_date = '-'.join([acquisition_date[:4], acquisition_date[4:6], acquisition_date[6:]])
+
     # Convert GeoJSON to WKT (Well-Known Text)
-    geometry_wkt = str(Polygon(geojson_polygon['coordinates']))
-    
+    wkt_geometry = f"POLYGON((\
+        {coordinates['top_left'].x} {coordinates['top_left'].y}, \
+        {coordinates['top_right'].x} {coordinates['top_right'].y}, \
+        {coordinates['bottom_right'].x} {coordinates['bottom_right'].y}, \
+        {coordinates['bottom_left'].x} {coordinates['bottom_left'].y}, \
+        {coordinates['top_left'].x} {coordinates['top_left'].y}\
+    ))"
+
+    image_path = str(image_path)
+    print("Image Path:", image_path)
+    print("Image Path Type", type(image_path))
+    print("Polygon",wkt_geometry)
+
     # Connect to the database
     conn = psycopg2.connect(**connection_params)
     cur = conn.cursor()
     
     # SQL command to insert new data
     cur.execute(
-        "INSERT INTO satellite_images (tile_id, acquisition_date, geometry, image_path) VALUES (%s, %s, ST_GeomFromText(%s, 4326), %s)",
-        (tile_id, acquisition_date, geometry_wkt, image_path)
+        "INSERT INTO satellite_images (tile_id, acquisition_date, geometry, image_path) VALUES (%s, %s, ST_GeomFromText(%s, 4326), %s)", (tile_id, acquisition_date, wkt_geometry, image_path)
     )
     
     # Commit changes
